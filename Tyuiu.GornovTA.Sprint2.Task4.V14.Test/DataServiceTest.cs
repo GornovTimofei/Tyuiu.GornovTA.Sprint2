3aebd31c60ab253cf49ce238786209fbc750b4c5
Tyuiu.GornovTA@outlook.com <Tyuiu.GornovTA@outlook.com>
2023-10-10 16:17:16 +0500

    Task4

---
+++ Tyuiu.GornovTA.Sprint2.Task4.V14.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.GornovTA.Sprint2.Task4.V14.Lib;
+
+namespace Tyuiu.GornovTA.Sprint2.Task4.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double y = 4;
+            double res = ds.Calculate(x, y);
+            double wait = 17.319;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 4;
+            double res = ds.Calculate(x, y);
+            double wait = 0.227;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}

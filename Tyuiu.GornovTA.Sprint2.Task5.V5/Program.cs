1734971c132e6b892f11991b8863f34d4cdbb99e
Tyuiu.GornovTA@outlook.com <Tyuiu.GornovTA@outlook.com>
2023-10-11 17:13:31 +0500

    Task5

---
+++ Tyuiu.GornovTA.Sprint2.Task5.V5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GornovTA.Sprint2.Task5.V5.Lib;
+
+namespace Tyuiu.GornovTA.Sprint2.Task5.V5
+{
+    class Program
+    {
+        static void Main()
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт №2 | Выполнил: Горнов Т. А. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт №2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание №5                                                              *");
+            Console.WriteLine("* Вариант №5                                                              *");
+            Console.WriteLine("* Выполнил: Горнов Тимофей Анатольевич | АСОиУб-23-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите порядковый номер карты: ");
+            int numCard = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((numCard < 6) || (numCard > 14))
+            {
+                res = "Введено неверное значение!";
+            }
+            else
+            {
+                res = "Это карта: " + ds.FindCardValue(numCard);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}

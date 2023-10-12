e0504ad54e73d13bc69114b3fde101ddcfe8c689
Tyuiu.GornovTA@outlook.com <Tyuiu.GornovTA@outlook.com>
2023-10-12 20:02:47 +0500

    Task6

---
+++ Tyuiu.GornovTA.Sprint2.Task6.V2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GornovTA.Sprint2.Task6.V2.Lib;
+
+namespace Tyuiu.GornovTA.Sprint2.Task6.V2
+{
+    class Program
+    {
+        static void Main()
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт №2 | Выполнил: Горнов Т. А. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт №2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание №6                                                              *");
+            Console.WriteLine("* Вариант №2                                                              *");
+            Console.WriteLine("* Выполнил: Горнов Тимофей Анатольевич | АСОиУб-23-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует сокращённую форму записи         *");
+            Console.WriteLine("* оператора switch вычесляет требуемое значение и возвращает результат.   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер месяца: ");
+            int numMouth = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((numMouth < 1) || (numMouth > 12))
+            {
+                res = "Введено неверное значение!";
+            }
+            else
+            {
+                res = "Это: " + ds.FindMonthSeason(numMouth);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
